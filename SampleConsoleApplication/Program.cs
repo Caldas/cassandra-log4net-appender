ca848fa7817e044b1ba7477e3cfeee2bca62fadb
Fabio Caldas <fabio.caldas@gmail.com>
2012-04-19 18:10:57 -0300

    Adjusts on Sample Application

--- SampleConsoleApplication/Program.cs
+++ SampleConsoleApplication/Program.cs
@@ -8,13 +8,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using log4net;
 
 namespace SampleConsoleApplication
 {
     class Program
     {
+        private static ILog Logger { get; set; }
+
         static void Main(string[] args)
         {
+            ConfigureLogger();
+
+            Logger.Info("TEST");
+        }
+
+        private static void ConfigureLogger()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            Logger = log4net.LogManager.GetLogger("WhiteBoard");
         }
     }
 }
